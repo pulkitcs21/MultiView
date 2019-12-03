df243a8345a4be2b485be5a129fd9d74b461e519
Pulkit <pulkit.agarwal@sjsu.edu>
2019-12-03 11:39:54 -0800

    test case

--- app/src/androidTest/java/com/pulkitagarwal/multicolumn/TextTest.java
+++ app/src/androidTest/java/com/pulkitagarwal/multicolumn/TextTest.java
@@ -2,12 +2,19 @@ package com.pulkitagarwal.multicolumn;
 
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.startsWith;
+
 
 public class TextTest {
 
-
     @Test
     public void testCheck(){
-        onView(withId(R.id.list1)).check(matches(isDisplayed()));
+        onData(hasToString(startsWith("Pizza")))
+                .inAdapterView(withId(R.id.listView))
+                .perform(click());
     }
 }
